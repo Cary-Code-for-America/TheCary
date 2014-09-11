df1bf609268b50242595387777ab1853fcaebc2d
Juan Orozco <juanthedesigner@gmail.com>
2014-09-11 17:27:45 -0400

    Add comments, improve readability.

--- server.js
+++ server.js
@@ -6,75 +6,78 @@ var moment  = require('moment');
 var app     = express();
 
 
-app.get('/scrape', function(req, res){
-    // The URL we will scrape from - in our example Anchorman 2.
+app.get('/scrape', function(req, res)
+{
 
-	url = 'http://www.townofcary.org/Departments/Parks__Recreation___Cultural_Resources/Facilities/Cultural_Arts_Centers/thecary/eventsandtickets/calendar.htm?';
+	var url = 'http://www.townofcary.org/Departments/Parks__Recreation___Cultural_Resources/Facilities/Cultural_Arts_Centers/thecary/eventsandtickets/calendar.htm?';
 
-	request(url, function(error, response, html){
+	request(url, function(error, response, html)
+  {
     // First we'll check to make sure no errors occurred when making the request
     if(!error)
     {
       // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
       var $ = cheerio.load(html);
+
+      //create base object for show times
       var listing = {scraped:[]};
       var cur_mom = moment();
 
+      //The table doesn't have any usefull attributes to help select it.
+      //Luckily, the CMS seems to have *some* structure that we can use to our advantage
+      //The screentimes seem to always be the only table within the main-content div.
       $("#main-content > div > table tbody").filter(function()
       {
         var data = $(this);
-        console.log( "Found " + data.find("tr").length);
+        //we are currently in the tbody tag and can iterate through each TR tag
         data.find("tr").each(function()
         {
+          //assume this TR is one show element
+          var row_data = [];//the current row's data
+          //let's loop through each TD tag int he current TR.
           var row = $(this).find("td");
-          var row_data = [];
           row.each(function()
           {
-            var cell = $(this).text();
+            var cell = $(this).text();//the current cell/TD's text
+            //just in case the cell is falsey?
             if (cell)
-              {
-                cell = cell.trim();
-                cell = cell.split("\r\n");
-                //cell = cell.replace(/\s{2}/g, " ");
-                if ( typeof cell === "object"  )
-                {
-                  for (var i = 0; i < cell.length; i++) {
-                    if (cell[i].trim() != "")
-                    {
-                      row_data.push( cell[i].trim() );
-                    }
-                  }
-                }
-                else
-                {
-                  if (cell != "")
-                  {
-                    row_data.push(cell);
+            {
+              //Some cells have multiple bits of info delimited by new lines
+              cell = cell.split("\r\n");
 
-                  }
+              //loop through array and add cell if not blank
+              for (var i = 0; i < cell.length; i++)
+              {
+                if (cell[i].trim() != "")
+                {
+                  row_data.push( cell[i].trim() );
                 }
               }
+            }
           });
+          //push the row's data to the main object
           listing.scraped.push(row_data);
         });
       });
+      //get today's date
       listing.scrape_date = cur_mom.format();
+      //create today's file name
       target_filename = "thecaryscrape_" + cur_mom.format("MMDDYYYY") + ".json";
+      //convert the object to string for file storage
       listing_string = JSON.stringify(listing, null, 4);
+      //store file
       fs.writeFile(target_filename, listing_string, function(err){
         console.log('File successfully written! - Check your project directory for the html file');
       })
 
-      // Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
+      //done, return response to user
       res.send(listing_string);
 		}
 
 	})
 })
 
-
+//port to listen on
 app.listen('8082')
-
-console.log('Magic happens on port 8082');
 
 exports = module.exports = app;
